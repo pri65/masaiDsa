0b252efde9ee1c8b4011b4ec21bbfd5d4ffa065c
pri65 <priyankasoniii65@gmail.com>
2022-06-05 18:35:36 +0530

    new

--- unit3/classes and constructors/classes.js
+++ unit3/classes and constructors/classes.js
@@ -6,6 +6,7 @@
 //     //method
 // }
 
+//1.eg
 class Person{
     constructor(name,city){
         this.name=name
@@ -16,4 +17,18 @@ var per1=new Person("Aman","Pune")
 var per2=new Person("Anu","Pune")
 var per3=new Person("Akash","Mumbai")
 var per4=new Person("Atul","Goa")
-console.log(per1,per2,per3);
+console.log(per1,per2,per3,per3);
+
+//2.eg (with method)
+class Person{
+    constructor(name,city){
+        this.name=name
+        this.city=city
+    }
+    welcome(){
+        return "Welcome"+ this.name+"from"+this.city;
+    }
+}
+var per1=new Person("Aman","Pune")
+var per2=new Person("Anu","Pune")
+console.log(per1,per2);
