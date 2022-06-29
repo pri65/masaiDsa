9eefa3e862d8eccea4c35a1ece39de7a60cc10e3
pri65 <priyankasoniii65@gmail.com>
2022-06-29 16:12:23 +0530

    new

---
+++ unit1/mathematical operators/findproduct.js
@@ -0,0 +1,10 @@
+// You are given five numbers stored in variables with the following names
+
+// one, two, three, four, five
+
+// Find the product of the five values, and print it
+
+
+function findProduct(one, two, three, four, five) {
+    console.log(one*two*three*four*five);
+}
