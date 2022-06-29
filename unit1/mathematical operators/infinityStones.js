7a0483e51aca0a4e3f339a0e861ffb5cdeeefffc
pri65 <priyankasoniii65@gmail.com>
2022-06-29 16:11:03 +0530

    new

---
+++ unit1/mathematical operators/infinityStones.js
@@ -0,0 +1,15 @@
+// You are given a number, stored in a variable with the namenumber. Perform the following operations on the value stored in the number
+
+// 1. Multiply the value by 3
+// 2. Add 7 after it
+// 3. Subtract 10 from it
+// After performing all the above operations, print the updated value
+
+
+// Input
+// // The first and the only line of the input contains the number, stored in the variablenumber
+
+
+function operationOnNumbers(number){
+    console.log(((number*3)+7)-10);
+}
---
+++ unit1/mathematical operators/squareIt.js
@@ -0,0 +1,6 @@
+// You are given an integer N, print its  [square value]
+
+function squareIt(N) {
+    console.log(N**2);
+    }
+    
