af98575f2dbf4eee67674026ff9e283aa1172bdd
pri65 <priyankasoniii65@gmail.com>
2022-07-03 22:58:16 +0530

    new

---
+++ unit2/covid_and_vowels.js
@@ -0,0 +1,38 @@
+// Description
+
+// Given a string S, find the number of substrings which contains all the vowels present in "coronavirus".
+
+
+// Input
+// Input Format
+
+// The single line consists of string S
+
+// Constraints
+
+// Length of S <=100
+
+
+// Output
+// Print count of substrings in a single line
+
+
+// Sample Input 1 
+
+// dangerouscovid
+// Sample Output 1
+
+// 4
+// Hint
+
+// Sample 1 Explanation
+
+// Out of all possible substrings following 4 substrings have all vowels [a,i,o,u]
+
+// dangerouscovi
+
+// dangerouscovid
+
+// angerouscovi
+
+// angerouscovid
