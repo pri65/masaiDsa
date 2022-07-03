3884ea56963eb6bf9d87f1cd614e6672f5ed98c8
pri65 <priyankasoniii65@gmail.com>
2022-07-03 22:31:05 +0530

    new

--- unit3/linked_list/insert_a_node_at_the_head.js
+++ unit3/linked_list/insert_a_node_at_the_head.js
@@ -34,3 +34,22 @@
 // 1
 // 2 1
 // 3 2 1
+
+
+
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+// Complete the function below
+
+function insertNodeAtHead(head, data) {
+    var node = new LinkedListNode(data); // 
+    node.next=head;//
+    head=node;
+    return head ; //{}
+}
+
