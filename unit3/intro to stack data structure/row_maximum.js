0bfa931f694b080e1f99a3e4e78f3b033421c26d
pri65 <priyankasoniii65@gmail.com>
2022-07-03 22:25:58 +0530

    new

---
+++ unit3/intro to stack data structure/row_maximum.js
@@ -0,0 +1,34 @@
+// Description
+
+// Given a matrix A of size n and m, you have to print the array of size n each element representing the maximum of ith row of the matrix A.
+
+
+// Input
+// Input Format
+
+// First-line contains 2 integers n and m representing the number of rows and columns respectively of 2D array arr.
+
+// Following n lines contains m-integers.
+
+// Constraints
+
+// 1 <= n <= 100
+
+// 1 <= m <= 100
+
+// 1 <= arr[i][j] <= 10^4
+
+
+// Output
+// Print the required array.
+
+
+// Sample Input 1 
+
+// 3 3
+// 1 2 3
+// 4 5 6
+// 7 8 9
+// Sample Output 1
+
+// 3 6 9
