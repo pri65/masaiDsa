a737ba7907bcd62b40bda19b995bed2e5ae86642
pri65 <priyankasoniii65@gmail.com>
2022-07-03 22:24:22 +0530

    new

--- unit3/intro to stack data structure/array_to_string.js
+++ unit3/intro to stack data structure/array_to_string.js
@@ -34,3 +34,45 @@
 // 20680
 
 
+function runProgram(input){
+    input=input.trim().split('\n')
+    var n=+input[0]
+    var arr=input[1].trim().split(" ").map(Number)
+    check(n,arr)
+ }
+ 
+ function check(n,arr){
+     var res=""
+     for(var i=0; i<n; i++)
+     {
+         if(arr[i]>=0)
+         {
+             res+=arr[i]
+         } else {
+             res+="0"
+         }
+     }
+     console.log(res)
+ }
+  
+ 
+ if (process.env.USER === "") {
+   runProgram(``);
+ } else {
+   process.stdin.resume();
+   process.stdin.setEncoding("ascii");
+   let read = "";
+   process.stdin.on("data", function (input) {
+     read += input;
+   });
+   process.stdin.on("end", function () {
+     read = read.replace(/\n$/, "");
+     read = read.replace(/\n$/, "");
+     runProgram(read);
+   });
+   process.on("SIGINT", function () {
+     read = read.replace(/\n$/, "");
+     runProgram(read);
+     process.exit(0);
+   });
+ }
