7e701e99d5a88bb8d351d2db8d04bd3e7562d06b
pri65 <priyankasoniii65@gmail.com>
2022-07-03 22:43:31 +0530

    new

---
+++ unit3/linked_list/newyork_skylines.js
@@ -0,0 +1,49 @@
+// Description
+
+// Gini and Johnny are on a trip to New York, and are in awe of the marvellous tall buildings of the city
+
+// So, they decided to play a game
+
+// They decided to give every building a rating, such that
+
+// If the building is greater than the building to its left, and the building to its right, it gets 2 point
+
+// If the building is greater than one of the buildings to it's left or the buildings to it right, it get's 1 point
+
+// If the building is smaller than the building to it's left and the building to it's right, it gets 0 point
+
+// Given an array, which gives the height of the buildings, find out the score of each building
+
+// Note : You don't have to take the input
+// Just complete the function and print the required function
+
+// Input
+// The first line of the input contains the valueT, the number of test cases
+
+// For each test case, the first line contains the valueN, the number of buildings
+
+// The next line contains the height of each building in the form ofNspace separated integers
+
+// Constraints
+
+// 1 <=T<= 10
+
+// 2 <=N<= 10000
+
+// 1 <=Height of building<= 1000
+
+
+// Output
+// For each test case, print the points scored by each of the buildings on a single line
+
+// Sample Input 1 
+
+// 2
+// 5
+// 1 4 3 2 7
+// 2
+// 1 3
+// Sample Output 1
+
+// 0 2 1 0 1 
+// 0 1 
