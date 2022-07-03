85612f1a9884e969a10114f48f07b410148d9310
pri65 <priyankasoniii65@gmail.com>
2022-07-03 22:39:43 +0530

    new

--- unit3/linked_list/middle_node.js
+++ unit3/linked_list/middle_node.js
@@ -34,3 +34,23 @@
 // Sample Output 1
 
 // 2
+
+
+
+const LinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+var middleNode = function(head) {
+    var slow=head;
+    var fast=head;
+    while(fast!=null && fast.next!=null) {
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+    return slow.data;
+};
+
