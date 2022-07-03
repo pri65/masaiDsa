9ae098ee3336b69eb69bcc11fdf95723eb75c13f
pri65 <priyankasoniii65@gmail.com>
2022-07-03 22:48:15 +0530

    new

---
+++ unit3/linked_list/pair_sum.js
@@ -0,0 +1,69 @@
+// Description
+
+// Given an array of N integers, you have to find the number of pairs of continuous elements whose sum is divisible by 2.
+
+// Example - 1, 2, 4, 4, 5
+
+// Sum of pairs of continuous elements are
+
+// 1+2 = 3
+// 2+4 = 6
+// 4+4 = 8
+// 4+5 = 9
+
+// See only two pairs are there with sum divisible by 2.
+
+
+// Input
+// Input Format
+
+// The first line contains integer numbers N - the size of the array.
+
+// The second-line contains N integer numbers a1, a2, ... aN — elements of the array.
+
+// Constraints
+
+// 1 <= N <= 10^4
+
+// 1 <= a[i] <= 10^4
+
+
+// Output
+// Print the number of such pairs.
+
+
+// Sample Input 1 
+
+// 5
+// 1 2 4 4 5
+// Sample Output 1
+
+// 2
+// Sample Input 2 
+
+// 5
+// 1 2 3 4 5
+// Sample Output 2
+
+// 0
+// Hint
+
+// For the first sample see the example explained in the question above.
+
+// For the second sample,
+
+// 1, 2, 3, 4, 5
+
+// Sum of pairs of continuous elements are
+
+// 1+2 = 3
+
+// 2+3 = 5
+
+// 3+4 = 7
+
+// 4+5 = 9
+
+// See no pairs are there with sum divisible by 2, therefore the answer is 0.
+
+
