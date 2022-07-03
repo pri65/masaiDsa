220b357bc21d2230a442fdd577331caca83db2bf
pri65 <priyankasoniii65@gmail.com>
2022-07-03 22:30:40 +0530

    new

--- unit3/linked_list/insert_a_node_at_the_head.js
+++ unit3/linked_list/insert_a_node_at_the_head.js
@@ -1,36 +1,36 @@
-Description
+// Description
 
-Given a pointer to the head of a linked list, insert a new node before the head. Return a reference to the new head of the list. The head pointer given may be null meaning that the initial list is empty.
+// Given a pointer to the head of a linked list, insert a new node before the head. Return a reference to the new head of the list. The head pointer given may be null meaning that the initial list is empty.
 
-Complete the function insertNodeAtHead in the editor below.
+// Complete the function insertNodeAtHead in the editor below.
 
-insertNodeAtHeadhas the following parameter(s):
+// insertNodeAtHeadhas the following parameter(s):
 
-LinkedListNode list: a reference to the head of a list
-data: the value to insert in the data field of the new node
+// LinkedListNode list: a reference to the head of a list
+// data: the value to insert in the data field of the new node
 
-Input
-The first line contains an integer n, the number of elements to be inserted at the head of the list.
+// Input
+// The first line contains an integer n, the number of elements to be inserted at the head of the list.
 
-The next n lines contain an integer each, the elements to be inserted, one per function call.
+// The next n lines contain an integer each, the elements to be inserted, one per function call.
 
-1 <= n <= 1000
+// 1 <= n <= 1000
 
-list[i] <= 1000
-
-
-Output
-For each case print the updated Linked List.
+// list[i] <= 1000
 
 
-Sample Input 1 
+// Output
+// For each case print the updated Linked List.
 
-3
-1
-2
-3
-Sample Output 1
 
-1
-2 1
-3 2 1
+// Sample Input 1 
+
+// 3
+// 1
+// 2
+// 3
+// Sample Output 1
+
+// 1
+// 2 1
+// 3 2 1
