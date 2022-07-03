d97785b3122c99773635efd3d9f37ceefce58486
pri65 <priyankasoniii65@gmail.com>
2022-07-03 22:49:37 +0530

    new

---
+++ unit3/linked_list/check_skewness.js
@@ -0,0 +1,35 @@
+// Description
+
+// Given an integer n, find out whether an integer is odd, even, or not skewed.
+
+// An integer is oddly skewed if it has more number of odd divisors than even divisors and vice versa for evenly skewed.
+
+// If the number of odd and even divisors are equal, then it is not skewed.
+
+// Refer to the sample testcase for I/O.
+
+
+// Input
+// The first line of the input contains one integer t (1 ≤ t ≤ 10) — the number of test cases. Then t test cases follow.
+
+// The first line of each test case contains a single integer n (1 ≤ n ≤ 100000000) — the number of shops in the neighborhood.
+
+
+// Output
+// For each test case, print the answer: "Odd Skewed" or "Even Skewed" or "Not Skewed".
+
+
+// Sample Input 1 
+
+// 3
+// 4
+// 6
+// 11
+// Sample Output 1
+
+// Even Skewed
+// Not Skewed
+// Odd Skewed
+
+
+
